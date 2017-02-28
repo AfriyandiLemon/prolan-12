1ed32901a7134b980a3dc5b30a5c7e743df27ebb
Afriyandi Jaya <redxii777@gmail.com>
2017-02-28 19:18:07 +0700

    Add files via upload

---
+++ Minggu 2/Mahasiswa.java
@@ -0,0 +1,8 @@
+/* Program Identitas */
+class Mahasiswa {
+public static void main (String  argv[] ){
+	System.out.println("Nama Lengkap : Afriyandi Jaya Pranata");
+	System.out.println("\nNIM : 10114215 ");
+	System.out.println("\nAlamat : Bandung ");
+	}
+	}
