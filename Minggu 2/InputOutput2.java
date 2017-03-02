1564c279d3371acaf9f399d9519aef221a25a0d2
Afriyandi Jaya <redxii777@gmail.com>
2017-03-02 20:51:52 +0700

    Add files via upload

---
+++ Minggu 2/InputOutput2.java
@@ -0,0 +1,7 @@
+class InputOutput2 {
+public static void main(String[] argv) {
+  System.out.print("Belajar Java Yuks  ");
+  System.out.print("\nBelajar Java Yuks 1");
+  System.out.print("\nBelajar Java Yuks 2");
+  }
+  }
---
+++ Minggu 2/InputOutput3.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class InputOutput3 {
+public static void main(String[] argv) {
+
+Scanner namas = new Scanner (System.in);
+String nama;
+System.out.print("nama :");
+nama = namas.nextLine();
+System.out.println(nama);
+}
+}
