71777ea4dc07067774da6e70512d340e83196d46
Afriyandi Jaya <redxii777@gmail.com>
2017-03-14 23:32:27 +0700

    Add files via upload

---
+++ Minggu 4/tabung.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class tabung {
+ int jumlah;
+
+Scanner input = new Scanner(System.in);
+
+tabung(){
+System.out.print("Masukkan jumlah tunai :");jumlah=input.nextInt();
+System.out.println("jumlah tunai :"+jumlah);
+}
+
+
+}
---
+++ Minggu 4/utama.java
@@ -0,0 +1,14 @@
+public class utama{
+	String nama;
+public static void main(String[] args) {
+	
+Nasabah Adi = new Nasabah();
+	Adi.menabung("Adi nugraha",400000);
+
+
+tabung ini = new tabung();
+
+Adi.menabung();
+
+}
+}
