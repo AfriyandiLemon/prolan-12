f924c44eb903f4785df557971845dbb92116f4c3
Afriyandi Jaya <redxii777@gmail.com>
2017-03-15 23:37:53 +0700

    Add files via upload

---
+++ Minggu 4/Minggu 4/Nasabah.java
@@ -0,0 +1,20 @@
+
+
+public class Nasabah {
+	String nama,bank,NoRek;
+	int jumlah;
+	
+void menabung (String nama,int jumlah){
+	
+	this.nama=nama;
+	this.jumlah=jumlah;
+	System.out.println("nama           : "+nama); 
+	System.out.println("Jumlah Setoran : "+jumlah);
+}	
+
+ void menabung (){
+ String nama;
+ nama="Terima kasih telah menggunakan jasa kami";
+ System.out.println(nama); }
+}
+	
