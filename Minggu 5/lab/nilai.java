50b376d87e1be084292a390d6c004c202fdce8fa
Afriyandi Jaya <redxii777@gmail.com>
2017-03-22 14:31:26 +0700

    Add files via upload

---
+++ Minggu 5/lab/nilai.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+public class nilai {
+public static void main(String argd[]) {
+Scanner input = new Scanner(System.in);
+int m;
+byte n,x=1;
+int minInt = -2147483648, maxInt=2147483647,max,min;
+char pil;
+
+	System.out.print("masukkan jumlah bilangan input :"); n=input.nextByte(); 
+		while (x<=n) {
+			System.out.print("Masukkan nilai ke-"+x+": ");m=input.nextInt();
+					x++;
+					}
+	if(m <= maxInt and m > 0){
+		pilihanA();
+		} else 
+	if (m<=minInt and m <0) {
+	pilihanB();
+	}
+	
+	System.out.print("masukkan pilihan:");pil=input.next().charAt(0);
+		if (pil = 'A'){
+			}
+void pilihanA(){
+	m=max;
+		if (m > max) {
+		max=m;
+	System.out.print("maksimum :" + max);	
+		} 
+};
+void pilihanB(){
+temp
+if (m<minInt){
+	}
+
+};
+
+
+										}	
+}
